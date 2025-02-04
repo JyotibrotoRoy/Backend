42f2473c1d6e86cd019514ba8bb5e272a3f5a178
Jyotibroto Roy <roylil675@gmail.com>
2025-02-05 00:27:02 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,13 +19,6 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
         fullname: {
             type: String,
             required: true,
@@ -58,7 +51,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
